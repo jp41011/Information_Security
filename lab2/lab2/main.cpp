6113c5bcf4f90c28798cded6a6ca2622ecb68779
jp41011 <Juan@MacBook-Pro-2.local>
2016-07-07 00:19:02 -0700

    Almost Finished with the basic request for lab2
    Need to find a better of doing the exponential calculations.

--- lab2/lab2/main.cpp
+++ lab2/lab2/main.cpp
@@ -16,7 +16,11 @@
 
 #include <iostream>
 #include <fstream>
-
+#include <vector>
+#include <string>
+#include <math.h>
+//#include <stdlib.h>
+#include <string>
 
 using namespace std;
 
@@ -24,15 +28,19 @@ using namespace std;
 // initiallaly hard coded but will eventually generate these values
 int P_val = 5;
 int Q_val = 7;
+
 int E_val = 5;
 int D_val = 29;
+int N_val = P_val * Q_val;
 
 //declare
-string RSAencryptString(string);
-string RSAdecryptString(string);
+vector<string> RSAencryptString(string);
+vector<string> RSAdecryptString(vector<string>);
 
-char RSAencryptChar(char);
-char RSAdecryptChar(char);
+string RSAencryptChar(char);
+string RSAdecryptChar(string);
+
+string vecToStr(vector<string>);
 
 int main(int argc, const char * argv[]) {
 	
@@ -47,15 +55,23 @@ int main(int argc, const char * argv[]) {
 	string plainText;
 	getline(plainFile, plainText);
 	
-	cout << plainText << endl;
+	cout << "Plain Text:\n\t" << plainText << endl;
 	
 	// call to encrypt the plain text using RSA
+	vector<string> eVec = RSAencryptString(plainText);
+	string eText = vecToStr(eVec);
 	
-	// save output to file
+	cout << "Encrypted Text:\n\t" << eText << endl;
+	
+	ecryptedFile << eText << endl; // save output to file
 	
 	// decrypte cipher text with the RSA
+	vector<string> dVec = RSAdecryptString(eVec);
+	string dText = vecToStr(dVec);
 	
-	// save decrypted text to file.
+	cout << "Decrypted Text:\n\t" << dText << endl;
+	
+	decryptedFile << dText << endl; // save decrypted text to file.
 	
 	
 	// clean up close all the files
@@ -73,12 +89,27 @@ int main(int argc, const char * argv[]) {
  Input:
  	string plainText - unecrypted plain text to be encrypted
  Output:
- 	string eString - RSA encrypted version of the string given string
+ 	vector<string> eString - RSA encrypted version of the string given string, each char is an element in the vector
 */
-string RSAencryptString(string plainText)
+vector<string> RSAencryptString(string plainText)
 {
-	string eString = ""; // to store generated ecrypted string
-
+	//string eString = ""; // to store generated ecrypted string
+	vector<string> eString (0);
+	
+	for(int i=0; i < plainText.size(); i++)
+	{
+		//char eChar = plainText[i];
+		if(isalpha(plainText[i]) == false) // non alpha char
+		{
+			//eString = eString + plainText[i] + " "; // add to eString and continue to next char
+			string tStr = string(1,plainText[i]);
+			eString.push_back(tStr);
+		}else{ // is an alpha char
+			//eString = eString + RSAencryptChar(plainText[i]) + " ";
+			eString.push_back(RSAencryptChar(plainText[i]));
+		}
+	}
+	
 	return eString;
 }
 
@@ -89,12 +120,79 @@ string RSAencryptString(string plainText)
  Output:
  	char eChar - encrypted version of the given char
 */
-char RSAencryptChar(char pChar)
+string RSAencryptChar(char pChar)
 {
-	char eChar = ' '; // to store the ecrypted char
+	string eChar = ""; // to store the ecrypted char ... which will be a number
+	if(isupper(pChar) == true) // if uppercase
+	{
+		pChar = tolower(pChar); // convert to lower case
+		//eChar = RSAencryptChar(tolower(pChar)); // convert to lowercase and recusive call
+		//return toupper(eChar);
+	}
+	//lower case
+	
+	//TODO: find more efficient way of doing calc
+	unsigned long long int num = pChar - (int('a')-1); // 'a' -> 1, 'b'->2 ...
+	
+	num = pow(num, E_val) ;//% N_val;
+	num = num % N_val;
+	
+	eChar = to_string(num); // convert int to string
 	
 	return eChar;
 }
 
-//string RSAdecryptString(string);
-//char RSAdecryptChar(char);
+/*
+ Input:
+ 	eVec - encrypted vector
+*/
+vector<string> RSAdecryptString(vector<string> eVec)
+{
+	vector<string> dVec (0); // vector with the decrypted characters
+	
+	for(int i=0; i < eVec.size(); i++)
+	{
+		string temp = RSAdecryptChar(eVec[i]);
+		dVec.push_back(temp);
+	}
+	
+	return dVec;
+}
+
+
+string RSAdecryptChar(string eStr)
+{
+	//int num = atoi(eStr);
+	int num = stoi(eStr, nullptr, 10); // convert string to int
+	if(num == 0) // was not a number
+	{
+		return eStr;
+	}
+	else
+	{
+		//TODO: find more efficient way of doing calculation
+		unsigned long long int num2 = pow(num, D_val);
+		num2 = num2 % N_val;
+		
+		char c = 'a' - 1 + num2;
+		
+		return string(1,c);
+	}
+}
+
+/*
+ Takes in a vector of strings and then iterates through the elemenents an add to a string to return. (space delimited)
+*/
+string vecToStr(vector<string> vec)
+{
+	string rString = "";
+	
+	for(int i=0; i < vec.size(); i++)
+	{
+		rString = rString + vec[i] + " ";
+	}
+	// now remove the last space at the end
+	rString = rString.substr(0, rString.size()-1);
+	
+	return rString;
+}
